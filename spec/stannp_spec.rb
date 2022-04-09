816c92af6204b8172b6a0981eda304e31aa251d2
k-p-jones <ken.jones@rocksteadymusicschool.com>
2022-04-09 14:07:53 +0100

    Add Stannp::Object spec

--- spec/stannp_spec.rb
+++ spec/stannp_spec.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe Stannp do
-  it "has a version number" do
+  it 'has a version number' do
     expect(Stannp::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(true).to eq(true)
   end
 end
